efa50ebcdcb4db658d3c2b725bd454db644a8c0b
Obakeng Masima <obakeng.masima@momentum.co.za>
2020-03-30 19:31:03 +0200

    initial commit

--- src/app/view/homepage/homepage.component.ts
+++ src/app/view/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TechService } from '../../config/services/tech.service';
+import { NewsModel } from '../../config/models/NewsDetails';
 
 @Component({
   selector: 'app-homepage',
@@ -8,6 +9,10 @@ import { TechService } from '../../config/services/tech.service';
 })
 export class HomepageComponent implements OnInit {
   response: any;
+  more: boolean;
+  article: NewsModel;
+  selectedArticle: any;
+  item: any;
 
   constructor(private Service: TechService) { }
 
@@ -15,6 +20,15 @@ export class HomepageComponent implements OnInit {
     this.Service.TechNews().subscribe((response) => {
       this.response = response;
     });
+    this.more = false;
+  }
+  readmore(item) {
+    this.more = true;
+    this.item = item;
+    console.log(this.item);
+  }
+  basicmodal() {
+
   }
 
 }
